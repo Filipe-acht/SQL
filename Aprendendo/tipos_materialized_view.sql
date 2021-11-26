7fc098374c025bdd97fc66166a8d86f9f985dcfb
Filipe N Costa Soares <58039037+Filipe-acht@users.noreply.github.com>
2021-11-26 14:22:42 -0300

    Curso finalizado

---
+++ Aprendendo/tipos_materialized_view.sql
@@ -0,0 +1,84 @@
+--Tipos de Visões Materializadas
+
+GRANT CREATE MATERIALIZED VIEW to hr; 
+
+-- Conectar como usuário hr
+
+-- Materialized Views com Funções de Agregação
+
+-- Criando Materialized View log -> para registrar as alterações na tabela 
+
+DROP MATERIALIZED VIEW LOG ON employees;
+
+CREATE MATERIALIZED VIEW LOG on employees;
+
+-- Criando Materialized View
+
+DROP MATERIALIZED VIEW mv_department_salaries;
+
+CREATE MATERIALIZED VIEW mv_department_salaries
+BUILD IMMEDIATE
+REFRESH COMPLETE
+ENABLE QUERY REWRITE
+AS 
+  SELECT e.department_id,
+         SUM(e.salary) sum_salary,
+         ROUND(AVG(NVL(e.salary,0)),2) avg_salary
+  FROM employees e
+  GROUP BY e.department_id;
+
+----------------------------
+SELECT *
+FROM   mv_department_salaries;
+
+-- Materialized Views contendo somente Joins
+
+-- Criando Materialized View
+
+DROP MATERIALIZED VIEW mv_employes_jobs_departments;
+
+CREATE MATERIALIZED VIEW mv_employes_jobs_departments
+BUILD IMMEDIATE
+REFRESH COMPLETE
+ENABLE QUERY REWRITE
+AS 
+SELECT e.employee_id
+     , e.first_name
+     , e.last_name
+     , e.salary
+     , e.commission_pct
+     , e.job_id, j.job_title
+     , e.department_id
+     , d.department_name
+FROM employees e
+     JOIN jobs j
+       ON (e.job_id = j.job_id)
+     JOIN departments d 
+       ON (e.department_id = d.department_id);
+
+----------------------------------
+     
+SELECT *
+FROM   mv_employes_jobs_departments;
+
+-- Nested Materialized Views -> é quando o select da view materializada referencia outra view materializada
+
+-- Criando Materialized View
+
+DROP MATERIALIZED VIEW mv_department_cust_hour;
+
+CREATE MATERIALIZED VIEW mv_department_cust_hour
+BUILD IMMEDIATE
+REFRESH COMPLETE
+ENABLE QUERY REWRITE
+AS 
+  SELECT mv.department_name,
+         SUM(mv.salary) sum_salary,
+         ROUND(SUM(mv.salary)/160,2) cust_hour
+  FROM   mv_employes_jobs_departments mv
+  GROUP BY mv.department_name;
+
+------------------------------------
+
+SELECT *
+FROM   mv_department_cust_hour;
